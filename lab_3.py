cb3aa485f71af9e2fc211bf7336464715cbcce54
Qlass <rafgor7@gmail.com>
2019-04-10 21:32:27 +0200

    Update lab_3.py
    changed

--- lab_3.py
+++ lab_3.py
@@ -11,49 +11,108 @@ from cs50 import get_float
 from numpy import pi
 
 
-def countField(figure):
-    y = 0
-    if figure == 'circle':
-        x = get_float("First number")
-    else:
-        x = get_float("First number")
-        y = get_float("Second number")
-    if figure == 'circle':
-        z = round(2 * pi * x, 2)
-        print("Field of circle: ", z)
-    elif figure == 'rectangle':
-        z = round(x * y, 2)
-        print("Field of rectangle: ", z)
-    elif figure == 'triangle':
-        z = round(1/2 * x * y, 2)
-        print("Field of rectangle: ", z)
-    else:
-        z = round(1 / 2 * x * y, 2)
-        print("Field of rhombus: ", z)
-    return z
+def field(tab):
+    name = tab[0].lower()
+    try:
+        x = tab[1]
+    except:
+        x = 0
+    try:
+        y = tab[2]
+    except:
+        y = 0
+    formula = {"a": 5, "circle": round(2 * pi * x,2), "rectangle": round(x*y,2), "triangle": round(1/2 * x *y,2), "rhombus": round(1/2*x*y,2)}
+    try:
+        return formula[name]
+    except KeyError:
+        return -1
 
-def compare():
+def figure(Tab):
     x = []
-    for i in range(2):
-        figure = 'a'
-        while figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
-            figure = str(input("Write: circle/rectangle/triangle/rhombus\n"))
-            if figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
-                print("You must write exacle this figure")
-        x.append([countField(figure),figure])
-    if x[0][0] == x[1][0]:
-        print("Both figure has a same field")
-    elif x.index(max(x)) == 0:
-        print("The first figure", max(x)[1], "has larger field")
+    for tab in Tab:
+        y = field(tab)
+        if y == 0:
+            print("Za mało argumentów w", tab[0])
+            return
+        elif y == -1:
+            print("Zła nazwa figury")
+            return
+        else:
+            if y > 0:
+                x.append(y)
+            else:
+                print("Nie mogą być mniejsze od zera")
+                return
+    if len(Tab) > 1:
+        maxx = max(x)
+        if x.index(maxx) == 0:
+            print("The first figure (", Tab[x.index(maxx)][0], ") has larger field")
+        elif x.index(maxx) == 1:
+            print("The second figure (", Tab[x.index(maxx)][0], ") has larger field")
+        else:
+            print("Both figure has a same field")
+        print(x)
     else:
-        print("The second figure", max(x)[1], "has larger field")
+        print(x[0])
 
-
-figure = 'a'
-while figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
-    figure = str(input("Write: circle/rectangle/triangle/rhombus\n"))
-    if figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
+figur = ""
+x = 0
+y = 0
+while figur != 'circle' and figur != 'rectangle' and figur != 'triangle' and figur != 'rhombus':
+    figur = str(input("Write: circle/rectangle/triangle/rhombus\n"))
+    if figur != 'circle' and figur != 'rectangle' and figur != 'triangle' and figur != 'rhombus':
         print("You must write exacle this figure")
-countField(figure)
+x = get_float("Podaj pierwszy argument ")
+y = get_float("Podaj drugi argument ")
+figure([[figur,x,y]])
 
-compare()
+x = 5
+y = 5
+figure([["triangle",x,y],["circle",5,5]])
+
+# def countField(figure):
+#     y = 0
+#     if figure == 'circle':
+#         x = get_float("First number")
+#     else:
+#         x = get_float("First number")
+#         y = get_float("Second number")
+#     if figure == 'circle':
+#         z = round(2 * pi * x, 2)
+#         print("Field of circle: ", z)
+#     elif figure == 'rectangle':
+#         z = round(x * y, 2)
+#         print("Field of rectangle: ", z)
+#     elif figure == 'triangle':
+#         z = round(1/2 * x * y, 2)
+#         print("Field of rectangle: ", z)
+#     else:
+#         z = round(1 / 2 * x * y, 2)
+#         print("Field of rhombus: ", z)
+#     return z
+#
+# def compare():
+#     x = []
+#     for i in range(2):
+#         figure = 'a'
+#         while figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
+#             figure = str(input("Write: circle/rectangle/triangle/rhombus\n"))
+#             if figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
+#                 print("You must write exacle this figure")
+#         x.append([countField(figure),figure])
+#     if x[0][0] == x[1][0]:
+#         print("Both figure has a same field")
+#     elif x.index(max(x)) == 0:
+#         print("The first figure", max(x)[1], "has larger field")
+#     else:
+#         print("The second figure", max(x)[1], "has larger field")
+#
+#
+# figure = 'a'
+# while figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
+#     figure = str(input("Write: circle/rectangle/triangle/rhombus\n"))
+#     if figure != 'circle' and figure != 'rectangle' and figure != 'triangle' and figure != 'rhombus':
+#         print("You must write exacle this figure")
+# countField(figure)
+#
+# compare()
